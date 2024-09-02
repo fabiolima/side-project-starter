e00b805a37f9febeecfb312278d033536af9367d
F. Lima <fabio.lima@local.com.br>
2024-09-01 22:28:11 -0300

    Test without domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
     authentication: :plain,
     address: ENV["SMTP_ADDRESS"],
     port: ENV["SMTP_PORT"],
-    domain: ENV["SMTP_DOMAIN"],
+    # domain: ENV["SMTP_DOMAIN"],
     user_name: ENV["SMTP_USER_NAME"],
     password: ENV["SMTP_PASSWORD"]
   }
