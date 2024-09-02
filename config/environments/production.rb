e86de9cb4daad9b4c122fc5a6c93fea867b58871
F. Lima <fabio.lima@local.com.br>
2024-09-01 21:47:22 -0300

    Add smtp host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: ENV["SMTP_HOST"] }
   config.action_mailer.smtp_settings = {
     authentication: :plain,
     address: ENV["SMTP_ADDRESS"],
