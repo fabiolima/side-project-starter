3ce92fae5a9b05e99927dbefe75b57c2911f0e25
F. Lima <fabio.lima@local.com.br>
2024-09-03 15:27:23 -0300

    update stripe customer when profile changes

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,4 +3,15 @@ class Profile < ApplicationRecord
 
   validates :first_name, presence: true, on: :update
   validates :last_name, presence: true, on: :update
+
+  after_update :update_stripe_customer
+
+  def update_stripe_customer
+    return unless saved_change_to_attribute?(:first_name) || saved_change_to_attribute?(:last_name)
+    Stripe::Customer.update(self.user.stripe_id, { name:  full_name })
+  end
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
